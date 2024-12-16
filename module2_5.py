21d93d36cbf9c19da5065b538389ef0e3faf2410
/c <ooodenieska123@gmail.com>
2024-12-16 13:35:21 +0300

    16/12

--- module2_5.py
+++ module2_5.py
@@ -2,7 +2,7 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         matrix.append([])
-        for j in range(m):
+        for _ in range(m):
             matrix[i].append(value)
     print(matrix)
 get_matrix(2, 2, 10)
