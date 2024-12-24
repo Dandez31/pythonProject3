7cc361de99a9b035e65164a67eade810a312c7e2
/c <ooodenieska123@gmail.com>
2024-12-24 11:14:27 +0300

    24/12

--- module5_1.py
+++ module5_1.py
@@ -1,10 +1,28 @@
-def test_function():
-    print("Hello World")
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        self.current_floor = 1
 
-    def inner_function():
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Наверное Вы ошиблись домом')
+            new_floor = int(input('На какой этаж поедем? : '))
+            return self.go_to(new_floor)
 
-      print("Я в области видимости функции test_function")
-    inner_function()
-test_function()
-inner_function()
+        else:
+            for floor in range(1, self.number_of_floors):
+                if floor == new_floor:
+                    print('<- Вы поднялись на этаж ', {new_floor})
+                else:
+                    print(f'{floor}')
+                    continue
+            print(f'<- последний этаж       ', {self.number_of_floors})
 
+
+h1 = House('ЖСК Горский', 18)
+h2 = House('Домик в деревне', 2)
+print(h1.name, h1.number_of_floors)
+
+new_floor = int(input('На какой этаж поедем? : '))
+h1.go_to(new_floor)
---
+++ module5_2.py
@@ -0,0 +1,21 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
+
+
+hightower = House('Башня', 12)
+warehouse = House('Склад', 4)
+
+print(hightower)
+print(warehouse)
+
+print(len(hightower))
+print(len(warehouse))
